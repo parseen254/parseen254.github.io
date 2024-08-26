21e53375d82d8ab1f2cd388b856804b6149c0f4d
David Parseen <hello@parseen.dev>
2024-08-26 07:16:07 +0300

    patch: attempt #2

--- svelte.config.js
+++ svelte.config.js
@@ -34,7 +34,9 @@ const config = {
 			handleHttpError: 'warn'
 		},
 		paths: {
-			assets: process.argv.includes('dev') ? '' : `https://${process.env.BASE_PATH}`,
+			assets: process.argv.includes('dev')
+				? ''
+				: `https:/${process.env.BASE_PATH}.github.io/{process.env.BASE_PATH}`,
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 			relative: false
 		}
